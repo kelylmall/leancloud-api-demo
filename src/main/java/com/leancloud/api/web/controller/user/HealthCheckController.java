019b1d7800f53fb91e5f1c6b666030d2e95114bf
kelylmall <kelylmall@gmail.com>
2016-12-13 15:01:47 +0800

    update checkping

--- src/main/java/com/leancloud/api/web/controller/user/HealthCheckController.java
+++ src/main/java/com/leancloud/api/web/controller/user/HealthCheckController.java
@@ -4,25 +4,23 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSONObject;
+import com.avos.avoscloud.internal.InternalConfigurationController;
+
 @RestController
 public class HealthCheckController {
-	@RequestMapping(value = "/")
-	@ResponseBody
-	public String check(@RequestHeader HttpHeaders headers,
-			@RequestParam(value = "key", required = false) String key,
-			@RequestBody(required = false) String requestBody) {
-		return "ok";
-	}
-
-	@RequestMapping(value = "/__engine/1/ping")
+	@RequestMapping(value = { "/", "/__engine/1/ping" })
 	@ResponseBody
 	public String checkPing(@RequestHeader HttpHeaders headers,
-			@RequestParam(value = "key", required = false) String key,
 			@RequestBody(required = false) String requestBody) {
-		return "ok";
+		JSONObject result = new JSONObject();
+		result.put("runtime", System.getProperty("java.version"));
+		result.put("version", InternalConfigurationController.globalInstance()
+				.getClientConfiguration().getUserAgent());
+		return result.toJSONString();
 	}
+
 }

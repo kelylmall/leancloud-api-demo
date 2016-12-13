efba332c5144853fc528c79650fdbf1e4d00dea5
kelylmall <kelylmall@gmail.com>
2016-12-13 14:09:04 +0800

    a

---
+++ src/main/java/com/leancloud/api/web/configuration/LeanCloudAppConfigurer.java
@@ -0,0 +1,38 @@
+package com.leancloud.api.web.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "leancloudApp", locations = "classpath:config/api-conf.properties")
+public class LeanCloudAppConfigurer {
+
+	private String appId;
+
+	private String appKey;
+
+	private String masterKey;
+
+	public String getAppId() {
+		return appId;
+	}
+
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+
+	public String getAppKey() {
+		return appKey;
+	}
+
+	public void setAppKey(String appKey) {
+		this.appKey = appKey;
+	}
+
+	public String getMasterKey() {
+		return masterKey;
+	}
+
+	public void setMasterKey(String masterKey) {
+		this.masterKey = masterKey;
+	}
+
+}

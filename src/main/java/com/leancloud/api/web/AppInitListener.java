bfbc3e62db8f41cf3bf60c87ea128fc707ca77d9
kelylmall <kelylmall@gmail.com>
2016-12-14 22:33:33 +0800

    fsdf

--- src/main/java/com/leancloud/api/web/AppInitListener.java
+++ src/main/java/com/leancloud/api/web/AppInitListener.java
@@ -6,24 +6,17 @@ import javax.servlet.annotation.WebListener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import cn.leancloud.LeanEngine;
 
 import com.avos.avoscloud.AVOSCloud;
 import com.avos.avoscloud.internal.impl.JavaRequestSignImplementation;
-import com.leancloud.api.web.configuration.LeanCloudAppConfigurer;
 
 @WebListener
-@Component
 public class AppInitListener implements ServletContextListener {
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(AppInitListener.class);
-
-	@Autowired
-	private LeanCloudAppConfigurer leanCloudAppConfigurer;
 
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
@@ -31,20 +24,28 @@ public class AppInitListener implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
-		logger.info("AppInitListener.contextInitialized--------------------------------------------");
-		String appId = leanCloudAppConfigurer.getAppId();
-		String appKey = leanCloudAppConfigurer.getAppKey();
-		String appMasterKey = leanCloudAppConfigurer.getMasterKey();
+		System.setProperty("LEANCLOUD_APP_PORT", "3000");
+		System.setProperty("LEANCLOUD_APP_ID",
+				"LrOxQaQGfylPVKwKyVbbEO2P-gzGzoHsz");
+		System.setProperty("LEANCLOUD_APP_KEY", "yq9OoOTM1wbewKIE4kWEwBHJ");
+		System.setProperty("LEANCLOUD_APP_MASTER_KEY",
+				"DCiz0tQOEGEhE662XbzjO0I1");
+		logger.info("AppInitListener.contextInitialized start--------------------------------------------");
+		String appId = System.getProperty("LEANCLOUD_APP_ID");
+		String appKey = System.getProperty("LEANCLOUD_APP_KEY");
+		String appMasterKey = System.getProperty("LEANCLOUD_APP_MASTER_KEY");
+		logger.info("appId:{},appKey:{},appMasterKey:{}", appId, appKey,
+				appMasterKey);
 		// 数据存储初始化
 		AVOSCloud.initialize(appId, appKey, appMasterKey);
 		AVOSCloud.setDebugLogEnabled(true);
 		// 云引擎初始化
 		try {
-			System.setProperty("LEANCLOUD_APP_PORT", "3000");
+
 			LeanEngine.initialize(appId, appKey, appMasterKey);
 			// 在请求签名中使用masterKey以激活云代码的最高权限
 			JavaRequestSignImplementation.instance().setUseMasterKey(true);
-			if (System.getenv("LEANCLOUD_APP_ENV").equals("development")) {
+			if ("development".equals(System.getProperty("LEANCLOUD_APP_ENV"))) {
 				// 如果是开发环境，则设置 AVCloud.callFunction 和 AVCloud.rpcFunction
 				// 调用本地云函数实现
 				// 如果需要本地开发时调用云端云函数实现，则注释掉下面语句。
@@ -53,5 +54,6 @@ public class AppInitListener implements ServletContextListener {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		logger.info("AppInitListener.contextInitialized end--------------------------------------------");
 	}
 }

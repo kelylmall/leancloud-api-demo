f3e935a1b702729bba2609280cd1963c5e6eb314
kelylmall <kelylmall@gmail.com>
2016-12-14 11:45:51 +0800

    dasdas

--- src/main/java/com/leancloud/api/web/ApiApplication.java
+++ src/main/java/com/leancloud/api/web/ApiApplication.java
@@ -9,8 +9,6 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import cn.leancloud.LeanEngine;
-
 import com.avos.avoscloud.AVOSCloud;
 import com.leancloud.api.web.configuration.LeanCloudAppConfigurer;
 
@@ -23,12 +21,14 @@ public class ApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) throws Exception {
 
-		String appId = System.getenv("LEANCLOUD_APP_ID");
-		String appKey = System.getenv("LEANCLOUD_APP_KEY");
-		String appMasterKey = System.getenv("LEANCLOUD_APP_MASTER_KEY");
-
+		// String appId = System.getenv("LEANCLOUD_APP_ID");
+		// String appKey = System.getenv("LEANCLOUD_APP_KEY");
+		// String appMasterKey = System.getenv("LEANCLOUD_APP_MASTER_KEY");
+		String appId = "LrOxQaQGfylPVKwKyVbbEO2P-gzGzoHsz";
+		String appKey = "yq9OoOTM1wbewKIE4kWEwBHJ";
+		String appMasterKey = "DCiz0tQOEGEhE662XbzjO0I1";
 		AVOSCloud.initialize(appId, appKey, appMasterKey);
-		LeanEngine.initialize(appId, appKey, appMasterKey);
+		// LeanEngine.initialize(appId, appKey, appMasterKey);
 		SpringApplication.run(ApiApplication.class, args);
 	}
 

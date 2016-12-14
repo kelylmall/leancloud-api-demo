bcff8f9dd2554befc90249b30013f45a54c6cd4d
kelylmall <kelylmall@gmail.com>
2016-12-14 11:45:31 +0800

    sdas

--- src/main/java/com/leancloud/api/web/controller/user/TestController.java
+++ src/main/java/com/leancloud/api/web/controller/user/TestController.java
@@ -8,9 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVObject;
+import com.leancloud.api.web.entity.Todo;
+
 @RestController
 @RequestMapping(value = "/api")
 public class TestController {
+
 	@RequestMapping(value = "/test")
 	@ResponseBody
 	public String test(@RequestHeader HttpHeaders headers,
@@ -20,4 +25,38 @@ public class TestController {
 		return "test";
 	}
 
+	@RequestMapping(value = "/testSaveDb")
+	@ResponseBody
+	public AVObject testSaveDb(@RequestHeader HttpHeaders headers,
+			@RequestParam(value = "key", required = false) String key,
+			@RequestBody(required = false) String requestBody) {
+		AVObject todo = new AVObject("Todo");
+		todo.put("title", "工程师周会");
+		todo.put("content", "每周工程师会议，周一下午2点");
+		try {
+			todo.save();
+			// 保存成功
+		} catch (AVException e) {
+			// 失败的话，请检查网络环境以及 SDK 配置是否正确
+			e.printStackTrace();
+		}
+		return todo;
+	}
+
+	@RequestMapping(value = "/testSaveDb1")
+	@ResponseBody
+	public Todo testSaveDb1(@RequestHeader HttpHeaders headers,
+			@RequestParam(value = "key", required = false) String key,
+			@RequestBody(required = false) String requestBody) {
+		Todo todo = new Todo();
+		todo.setContent("222222");
+		try {
+			todo.save();
+			// 保存成功
+		} catch (AVException e) {
+			// 失败的话，请检查网络环境以及 SDK 配置是否正确
+			e.printStackTrace();
+		}
+		return todo;
+	}
 }

bb1f57b6f265066d5207e2645ef7bb8aa9183df1
kelylmall <kelylmall@gmail.com>
2016-12-19 17:53:23 +0800

    sdfasdf

--- src/main/java/com/leancloud/api/web/function/jni/HelloWorld.java
+++ src/main/java/com/leancloud/api/web/function/jni/HelloWorld.java
@@ -5,12 +5,16 @@ import java.lang.reflect.Field;
 public class HelloWorld {
 	public native void hello();
 
+	public native String testJni(String keyword);
+
 	public static void main(String[] args) throws NoSuchFieldException,
 			SecurityException, IllegalArgumentException, IllegalAccessException {
 		System.out.println("test");
 		// 设置查找路径为当前项目路径
-		System.setProperty("java.library.path",
-				System.getProperty("java.library.path")+ ":/Users/kelylmall/documents/workspace4/leancloud-api-demo/src/main/resources");
+		System.setProperty(
+				"java.library.path",
+				System.getProperty("java.library.path")
+						+ ":/Users/kelylmall/documents/workspace4/leancloud-api-demo/src/main/resources");
 		ClassLoader.class.getDeclaredField("sys_paths");
 		Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
 		fieldSysPath.setAccessible(true);

1a593810a645457f831dec53d5d233b8a6631d44
kelylmall <kelylmall@gmail.com>
2016-12-21 21:01:45 +0800

    dfafasdf

--- src/main/java/com/leancloud/api/web/task/ScheduledTask.java
+++ src/main/java/com/leancloud/api/web/task/ScheduledTask.java
@@ -18,9 +18,8 @@ public class ScheduledTask {
 	private Integer count = 1;
 
 	// cron = "*/5 * * * * * *"提供了一种通用的定时任务表达式，这里表示每隔5秒执行一次，更加详细的信息可以参考linux
-	// cron表达式
-	@Scheduled(cron = "0 */1 *  * * *")
 	// 每秒执行执行一次
+	@Scheduled(cron = "0 */1 *  * * *")
 	public void reportCurrentTimeCron() {
 		try {
 			logger.info("+++第{}次执行，当前时间为：{}", count++,

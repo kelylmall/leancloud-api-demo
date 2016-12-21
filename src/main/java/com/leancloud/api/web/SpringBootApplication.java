b36efe11ec502b785bc81b2c26e55735036816e0
kelylmall <kelylmall@gmail.com>
2016-12-21 18:52:27 +0800

    dasdasd

--- src/main/java/com/leancloud/api/web/SpringBootApplication.java
+++ src/main/java/com/leancloud/api/web/SpringBootApplication.java
@@ -19,10 +19,10 @@ import org.springframework.web.context.WebApplicationContext;
 
 import cn.leancloud.LeanEngine;
 
+import com.avos.avoscloud.AVOSCloud;
 import com.avos.avoscloud.internal.impl.JavaRequestSignImplementation;
 import com.leancloud.api.web.configuration.LeanCloudAppConfigurer;
 import com.leancloud.api.web.function.TestFuntion;
-import com.leancloud.api.web.function.jni.HelloWorld;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -36,38 +36,46 @@ public class SpringBootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) throws Exception {
 		logger.debug("SpringBootApplication.run---start---");
-		SpringApplication application = new SpringApplication(
-				SpringBootApplication.class);
+		initCloud();
+		SpringApplication application = new SpringApplication(SpringBootApplication.class);
 		application.run();
-	}
-
-	private static void loadJNILibDynamically() {
-		try {
-			System.setProperty(
-					"java.library.path",
-					System.getProperty("java.library.path")
-							+ ":/Users/kelylmall/documents/workspace4/leancloud-api-demo/src/main/resources");
-			ClassLoader.class.getDeclaredField("sys_paths");
-			Field fieldSysPath = ClassLoader.class
-					.getDeclaredField("sys_paths");
-			fieldSysPath.setAccessible(true);
-			fieldSysPath.set(null, null);
-			System.loadLibrary("hello");
-			HelloWorld helloWorld = new HelloWorld();
-			helloWorld.hello();
-			System.out.println(System.getProperty("java.library.path"));
-		} catch (Exception e) {
-			// do nothing for exception
-		}
 	}
 
 	@Override
 	public void onStartup(ServletContext servletContext)
 			throws ServletException {
+		initLoadJniLib(servletContext);
+		super.onStartup(servletContext);
+	}
+
+	@Override
+	protected WebApplicationContext createRootApplicationContext(
+			ServletContext servletContext) {
+		return super.createRootApplicationContext(servletContext);
+	}
+
+	@Override
+	protected SpringApplicationBuilder createSpringApplicationBuilder() {
+		return super.createSpringApplicationBuilder();
+	}
+
+	@Override
+	protected WebApplicationContext run(SpringApplication application) {
+		initCloud();
+		return super.run(application);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder application) {
+		return application.sources(getClass());
+	}
+
+	public static void initLoadJniLib(ServletContext servletContext) {
+		logger.debug("SpringBootApplication.initLoadJniLib---start---");
 		// jni_lib库目录
 		String contextPath = servletContext.getRealPath("/");
 		String jni_lib_path = contextPath + "/jni_lib";
-
 		System.setProperty("java.library.path",
 				System.getProperty("java.library.path") + ":" + jni_lib_path);
 		logger.info("SpringBootApplication.onStartup jni_lib_path:"
@@ -88,23 +96,11 @@ public class SpringBootApplication extends SpringBootServletInitializer {
 		} catch (IllegalAccessException e) {
 			e.printStackTrace();
 		}
-		super.onStartup(servletContext);
+		logger.debug("SpringBootApplication.initLoadJniLib---end---");
 	}
 
-	@Override
-	protected WebApplicationContext createRootApplicationContext(
-			ServletContext servletContext) {
-		return super.createRootApplicationContext(servletContext);
-	}
-
-	@Override
-	protected SpringApplicationBuilder createSpringApplicationBuilder() {
-		return super.createSpringApplicationBuilder();
-	}
-
-	@Override
-	protected WebApplicationContext run(SpringApplication application) {
-		logger.info("SpringBootApplication.run start--------------------------------------------");
+	public static void initCloud() {
+		logger.info("SpringBootApplication.initCloud------ start----------");
 		System.setProperty("LEANCLOUD_APP_PORT", "3000");
 		System.setProperty("LEANCLOUD_APP_ID",
 				"LrOxQaQGfylPVKwKyVbbEO2P-gzGzoHsz");
@@ -123,7 +119,7 @@ public class SpringBootApplication extends SpringBootServletInitializer {
 		// 在请求签名中使用masterKey以激活云代码的最高权限
 		JavaRequestSignImplementation.instance().setUseMasterKey(true);
 		// 打开 debug 日志
-		// AVOSCloud.setDebugLogEnabled(true);
+		AVOSCloud.setDebugLogEnabled(true);
 		// 向云引擎注册云函数
 		LeanEngine.register(TestFuntion.class);
 		if ("development".equals(System.getenv("LEANCLOUD_APP_ENV"))) {
@@ -131,14 +127,7 @@ public class SpringBootApplication extends SpringBootServletInitializer {
 			// 如果需要本地开发时调用云端云函数实现，则注释掉下面语句。
 			LeanEngine.setLocalEngineCallEnabled(true);
 		}
-
-		return super.run(application);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(
-			SpringApplicationBuilder application) {
-		return application.sources(getClass());
+		logger.info("SpringBootApplication.initCloud end --------------------------------------------------------");
 	}
 
 }
--- src/main/java/com/leancloud/api/web/controller/user/TestController.java
+++ src/main/java/com/leancloud/api/web/controller/user/TestController.java
@@ -1,7 +1,10 @@
 package com.leancloud.api.web.controller.user;
 
+import java.util.HashMap;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -9,15 +12,19 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVFile;
 import com.avos.avoscloud.AVObject;
-import com.leancloud.api.web.entity.Todo;
 import com.leancloud.api.web.function.jni.HelloWorld;
+import com.leancloud.api.web.resp.BaseResp;
+import com.leancloud.api.web.resp.ClientStateCode;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -31,15 +38,17 @@ public class TestController {
 			@RequestParam(value = "key", required = false) String key,
 			@RequestBody(required = false) String requestBody,
 			HttpServletRequest request) {
-		String realPath = request.getSession().getServletContext().getRealPath("/");
+		String realPath = request.getSession().getServletContext()
+				.getRealPath("/");
 		new HelloWorld().hello();
 		return realPath;
 	}
 
 	@RequestMapping(value = "/testJni")
 	@ResponseBody
-	public String testJni(@RequestHeader HttpHeaders headers,
-			@RequestParam(value = "key", required = false,defaultValue="jni") String key,
+	public String testJni(
+			@RequestHeader HttpHeaders headers,
+			@RequestParam(value = "key", required = false, defaultValue = "jni") String key,
 			@RequestBody(required = false) String requestBody) {
 		String testJni = new HelloWorld().testJni(key);
 		return testJni;
@@ -58,17 +67,54 @@ public class TestController {
 		return todo;
 	}
 
-	@RequestMapping(value = "/testSaveDb1", produces = { MediaType.APPLICATION_JSON_VALUE })
+	// 从数据流构建文件
+	@RequestMapping(value = "/uploadFile", produces = { MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.POST)
 	@ResponseBody
-	public Todo testSaveDb1(@RequestHeader HttpHeaders headers,
-			@RequestParam(value = "key", required = false) String key,
-			@RequestBody(required = false) String requestBody)
-			throws AVException {
-		Todo todo = new Todo();
-		todo.setContent("222222");
-		logger.debug(JSON.toJSONString(todo));
-		todo.save();
-		return todo;
+	public BaseResp uploadFile(@RequestHeader HttpHeaders headers,
+			@RequestParam(value = "file", required = true) MultipartFile file,
+			HttpServletRequest request) throws Exception {
+		boolean multipartContent = ServletFileUpload
+				.isMultipartContent(request);// 检查输入请求是否为multipart
+		BaseResp baseResp = null;
+		if (multipartContent) {
+			if (file == null) {
+				baseResp = new BaseResp(ClientStateCode.file_empty);
+			} else {
+				String originalFilename = file.getOriginalFilename();
+				AVFile avFile = new AVFile(originalFilename, file.getBytes());
+				avFile.save();
+				String url = avFile.getUrl();
+				String objectId = avFile.getObjectId();
+				JSONObject result = new JSONObject();
+				result.put("url", url);
+				result.put("objectId", objectId);
+				baseResp = new BaseResp(ClientStateCode.SUCCESS);
+				baseResp.setData(result);
+			}
+		} else {
+			baseResp = new BaseResp(ClientStateCode.uploadfile_type_err);
+		}
+		return baseResp;
+	}
+
+	// 上传网络图片
+	@RequestMapping(value = "/uploadNetworkFile", produces = { MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.POST)
+	@ResponseBody
+	public BaseResp uploadNetworkFile(@RequestHeader HttpHeaders headers,
+			HttpServletRequest request) throws Exception {
+		AVFile avFile = new AVFile(
+				"test.gif",
+				"http://ww3.sinaimg.cn/bmiddle/596b0666gw1ed70eavm5tg20bq06m7wi.gif",
+				new HashMap<String, Object>());
+		avFile.save();
+		String url = avFile.getUrl();
+		String objectId = avFile.getObjectId();
+		JSONObject result = new JSONObject();
+		result.put("url", url);
+		result.put("objectId", objectId);
+		BaseResp baseResp = new BaseResp(ClientStateCode.SUCCESS);
+		baseResp.setData(result);
+		return baseResp;
 	}
 
 }

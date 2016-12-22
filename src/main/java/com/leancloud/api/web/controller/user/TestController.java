37e3166e02f30a66d95e76cc1871dddb64a98371
kelylmall <kelylmall@gmail.com>
2016-12-22 15:50:06 +0800

    FADS

--- src/main/java/com/leancloud/api/web/controller/user/TestController.java
+++ src/main/java/com/leancloud/api/web/controller/user/TestController.java
@@ -1,12 +1,15 @@
 package com.leancloud.api.web.controller.user;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.apache.http.protocol.HTTP;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.InputStreamResource;
@@ -23,13 +26,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVFile;
 import com.avos.avoscloud.AVObject;
-import com.avos.avoscloud.GetDataCallback;
-import com.avos.avoscloud.GetFileCallback;
-import com.avos.avoscloud.ProgressCallback;
 import com.leancloud.api.web.function.jni.HelloWorld;
 import com.leancloud.api.web.resp.BaseResp;
 import com.leancloud.api.web.resp.ClientStateCode;
@@ -126,29 +127,77 @@ public class TestController {
 		return baseResp;
 	}
 
-	@RequestMapping(value = "/download/{objectId}", method = RequestMethod.GET)
-	public ResponseEntity<InputStreamResource> downloadFile(
-			@PathVariable("objectId") String objectId) throws IOException,
-			AVException {
+	// 从数据流构建文件上传
+	@RequestMapping(value = "/uploadStringFile", produces = { MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.POST)
+	@ResponseBody
+	public BaseResp uploadFile(@RequestHeader HttpHeaders headers,
+			HttpServletRequest request) throws Exception {
+		AVFile avFile = new AVFile("test.txt", "sdasdasdasdas".getBytes());
+		avFile.save();
+		String url = avFile.getUrl();
+		String objectId = avFile.getObjectId();
+		JSONObject result = new JSONObject();
+		result.put("url", url);
+		result.put("objectId", objectId);
+		BaseResp baseResp = new BaseResp(ClientStateCode.SUCCESS);
+		baseResp.setData(result);
 
-		AVFile avFile = AVFile.withObjectId(objectId);
-		byte[] data = avFile.getData();
-	
-		// // query String filePath = id + ".rmvb";
-		// FileSystemResource file = new FileSystemResource(filePath);
-		// HttpHeaders headers = new HttpHeaders();
-		// headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-		// headers.add("Content-Disposition",
-		// String.format("attachment; filename=\"%s\"", file.getFilename()));
-		// headers.add("Pragma", "no-cache");
-		// headers.add("Expires", "0");
-		// return ResponseEntity
-		// .ok()
-		// .headers(headers)
-		// .contentLength(file.contentLength())
-		// .contentType(MediaType.parseMediaType("application/octet-stream"))
-		// .body(new InputStreamResource(file.getInputStream()));
+		return baseResp;
+	}
+
+	@RequestMapping(value = "/download/{objectId}", method = RequestMethod.GET)
+	public ResponseEntity<Object> downloadFile(
+			@PathVariable("objectId") String objectId) {
+
+		AVFile avFile = null;
+		try {
+			avFile = AVFile.withObjectId(objectId);
+		} catch (FileNotFoundException | AVException e1) {
+			e1.printStackTrace();
+
+			BaseResp baseResp = new BaseResp(ClientStateCode.file_not_exists);
+			return ResponseEntity
+					.ok()
+					.contentType(
+							MediaType
+									.parseMediaType("application/json;charset=UTF-8"))
+					.body(JSON.toJSONString(baseResp));
+		}
+
+		String originalName = avFile.getOriginalName();
+		int size = avFile.getSize();
+		String fileUrl = avFile.getUrl();
+		InputStream inputStream = null;
+		try {
+			inputStream = getInputStream(fileUrl);
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+			headers.add("Content-Disposition",
+					String.format("attachment; filename=\"%s\"", originalName));
+			headers.add("Pragma", "no-cache");
+			headers.add("Expires", "0");
+			return ResponseEntity
+					.ok()
+					.headers(headers)
+					.contentLength(size)
+					.contentType(
+							MediaType
+									.parseMediaType("application/octet-stream"))
+					.body(new InputStreamResource(inputStream));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return null;
+	}
+
+	private InputStream getInputStream(String fileUrl) throws IOException {
+		URL url;
+		url = new URL(fileUrl);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setConnectTimeout(30 * 1000);
+		InputStream inputStream = conn.getInputStream();// 通过输入流获取图片数据
+		return inputStream;
 	}
 
 }

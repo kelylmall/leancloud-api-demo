7dc2f96ae7f1a93cd6f4c4ca727cfefc833f1095
kelylmall <kelylmall@gmail.com>
2016-12-22 16:14:59 +0800

    code

--- src/main/java/com/leancloud/api/web/resp/ClientStateCode.java
+++ src/main/java/com/leancloud/api/web/resp/ClientStateCode.java
@@ -1,10 +1,13 @@
 package com.leancloud.api.web.resp;
 
+import java.io.File;
+
 public enum ClientStateCode {
 	SUCCESS("00000","success"),
     SYSTEM_ERR("00001","system err"),
     file_empty("10001","file empty"),
     uploadfile_type_err("10002", "Content-Type not 'application/octet-stream' "),
+    file_not_exists("10003","file not exists"),
 	;
 	
 	
